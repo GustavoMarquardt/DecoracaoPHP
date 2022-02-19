9ba44ff11fa2930690b494519edfb7d329ef5c25
Gustavo Marquardt <gustavo.marquardt@unifei.edu.br>
2022-02-18 21:54:15 -0300

    projeto decoracao first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title> Decoração presentes</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body class="body">
+    <div class="topnav">
+    <h3 class="login"><a HREF="login.php?p=login" style="text-decoration: none; color:white">Login</a></h3>
+        <div class="imgLogo">
+            <img src="imgProject/semFundo.png" width="400px" height="120px">
+            <input type="text" name="pesquisa" class="pesquisa" placeholder="O que você esta procurando?">  
+        </div>
+</body>
+
+</html>
